18aa74337461dae1ae971454dc1054d7142a83f3
sandhya <sandhya@gmail.com>
2024-09-19 14:30:07 +0530

    commit

--- file.php
+++ file.php
@@ -1,14 +1,30 @@
 <?php
-$a = 5;
-if($a>15){
-    echo"condition.1 true";
-}else if($a>10){
-    echo"condition.2 true";
-}else if($a>5){
-    echo"condition.3 true";
-}else{
-    echo"error";
+// if(isset($_POST['submit'])){
+//     $name=$_POST['Fname'];
+//     $num=$_POST['Pnumber'];
+    
+//     echo "<script>document.write('$name, $num');</script>";
+// };
+
+$a = 120;
+while ($a<= 140) {
+    echo "$a ";
+    $a *= 3;
 }
-
-
 ?>
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="POST">
+    <input type="text" name="Fname" placeholder="Enter Your Name"><br>
+    <input type="number" name="Pnumber" placeholder="Enter Your Number"><br>  
+    <button name="submit">submit</button>       
+</form>
+    
+</body>
+</html> -->
