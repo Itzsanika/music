c4db5cd5fd0021765b0ea6f21fdf631fb5acc741
sandhya <sandhya@gmail.com>
2024-09-19 14:29:51 +0530

    commit

--- file.php
+++ file.php
@@ -1,10 +1,14 @@
 <?php
-$a = 20
-if($a>=15){
-    echo"condition trur";
+$a = 5;
+if($a>15){
+    echo"condition.1 true";
+}else if($a>10){
+    echo"condition.2 true";
+}else if($a>5){
+    echo"condition.3 true";
 }else{
-    echo"condition false";
-
+    echo"error";
+}
 
 
 ?>
